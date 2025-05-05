ce51775549c59bf3b40cfc5048968a91936f1cb2
gv-sothebys-arnarps <arnarpall.skulason.consultant@sothebys.com>
2025-05-05 20:27:45 +0000

    Remove unwanted code

--- libs/api/domains/skattskil/src/lib/api-domains-skattskil.module.ts
+++ libs/api/domains/skattskil/src/lib/api-domains-skattskil.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
 import { SkattskilService } from './skattskil.service';
 import { SkattskilResolver } from './skattskil.resolver';
-import { ElasticModule } from '@island.is/api-catalogue/elastic'
-import { ApiCatalogueServicesModule } from '@island.is/api-catalogue/services'
 
 
 @Module({
-  imports: [ElasticModule, ApiCatalogueServicesModule],
+  imports: [],
   providers: [SkattskilResolver, SkattskilService],
   exports: [],
 })
