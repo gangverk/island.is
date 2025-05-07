47ba025a5630e3e55525089bc52682bcc8f70e6b
Pall Valmundsson <pall.valmundsson@gmail.com>
2025-05-07 12:23:48 +0000

    Add mutations to skattskil api module

--- libs/api/domains/skattskil/src/lib/api-domains-skattskil.module.ts
+++ libs/api/domains/skattskil/src/lib/api-domains-skattskil.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 import { SkattskilService } from './skattskil.service';
-import { SkattskilResolver, TaxPayerResolver, TaxReturnResolver } from './skattskil.resolver';
+import { SkattskilResolver, TaxPayerResolver, TaxReturnResolver, TaxReturnIncomeResolver } from './skattskil.resolver';
 
 
 @Module({
   imports: [],
-  providers: [SkattskilResolver, TaxPayerResolver, TaxReturnResolver, SkattskilService],
+  providers: [SkattskilResolver, TaxPayerResolver, TaxReturnResolver, TaxReturnIncomeResolver, SkattskilService],
   exports: [],
 })
 export class ApiDomainsSkattskilModule {}
