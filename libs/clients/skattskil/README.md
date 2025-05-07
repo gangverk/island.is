b6f17d82b341b6f2c6114235c13e2087192e0447
Pall Valmundsson <3846899+pall-valmundsson@users.noreply.github.com>
2025-05-07 19:33:40 +0000

    Make skattskil income mutations use the skattskil service (#57)

---
+++ libs/clients/skattskil/README.md
@@ -0,0 +1,5 @@
+## Generate client
+```
+yarn nx run services-skattskil:codegen/backend-schema
+yarn nx run clients-skattskil:codegen/backend-client
+```
--- libs/clients/skattskil/src/lib/SkattskilClientService.ts
+++ libs/clients/skattskil/src/lib/SkattskilClientService.ts
@@ -5,14 +5,6 @@ import {
 } from '../../gen/fetch/apis'
 import { SkattskilClientConfig } from './SkattskilClientConfig'
 import { ConfigType } from '@nestjs/config'
-//import {
-//  CreateDraftRegulationCancelDto,
-//  CreateDraftRegulationChangeDto,
-//  CreateDraftRegulationDto,
-//  UpdateDraftRegulationCancelDto,
-//  UpdateDraftRegulationChangeDto,
-//  UpdateDraftRegulationDto,
-//} from '../../gen/fetch/models'
 
 export class SkattskilClientService {
   constructor(
@@ -44,6 +36,84 @@ export class SkattskilClientService {
     return this.skattskilApi.skattskilControllerGetTaxReturnById({
       id,
     })
+  }
+
+  getTaxReturnsByTaxPayerId(taxPayerId: string) {
+    return this.skattskilApi.skattskilControllerGetTaxReturnsByTaxPayerId({
+      taxPayerId,
+    })
+  }
+
+  addTaxReturnSalaryIncome(taxReturnId: string, description: string, amount: number, payer: string) {
+    return this.skattskilApi.skattskilControllerCreateSalaryIncome({
+      taxReturnId,
+      incomeInputDTO: {
+        description: description,
+        amount: amount,
+        payer: payer,
+      },
+    })
+  }
+
+  addTaxReturnGrantIncome(taxReturnId: string, description: string, amount: number, payer: string) {
+    return this.skattskilApi.skattskilControllerCreateGrantIncome({
+      taxReturnId,
+      incomeInputDTO: {
+        description: description,
+        amount: amount,
+        payer: payer,
+      },
+    })
+  }
+
+  addTaxReturnPerDiemIncome(taxReturnId: string, description: string, amount: number, payer: string) {
+    return this.skattskilApi.skattskilControllerCreatePerDiemIncome({
+      taxReturnId,
+      incomeInputDTO: {
+        description: description,
+        amount: amount,
+        payer: payer,
+      },
+    })
+  }
+
+  updateTaxReturnSalaryIncome(incomeId: string, description: string, amount: number, payer: string) {
+    return this.skattskilApi.skattskilControllerUpdateSalaryIncome({
+      id: incomeId,
+      incomeInputDTO: {
+        description: description,
+        amount: amount,
+        payer: payer,
+      },
+    })
+  }
+
+  updateTaxReturnPerDiemIncome(incomeId: string, description: string, amount: number, payer: string) {
+    return this.skattskilApi.skattskilControllerUpdatePerDiemIncome({
+      id: incomeId,
+      incomeInputDTO: {
+        description: description,
+        amount: amount,
+        payer: payer,
+      },
+    })
+  }
+
+  updateTaxReturnGrantIncome(incomeId: string, description: string, amount: number, payer: string) {
+    return this.skattskilApi.skattskilControllerUpdateGrantIncome({
+      id: incomeId,
+      incomeInputDTO: {
+        description: description,
+        amount: amount,
+        payer: payer,
+      },
+    })
+  }
+
+  deleteTaxReturnIncome(incomeId: string) {
+    //return this.skattskilApi.skattskilControllerDeleteIncome({
+    //  id: incomeId,
+    //})
   }
 
   //getShippedRegulations(auth: Auth) {
