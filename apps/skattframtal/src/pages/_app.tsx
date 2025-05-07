a19a312a269d369723cf1938ff67bc66a661020c
Jón Freysteinn Jónsson <nonni95@gmail.com>
2025-05-07 11:01:25 +0000

    feat(skattframtal): add apollo client

--- apps/skattframtal/src/pages/_app.tsx
+++ apps/skattframtal/src/pages/_app.tsx
@@ -4,20 +4,24 @@ import { Footer } from '../components/Footer'
 import './styles.css'
 import Header from '../components/Header/Header'
 import { globalStyles } from '@island.is/island-ui/core'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@island.is/application/graphql'
 
 globalStyles()
 
 const CustomApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Head>
-        <title>Welcome to skattframtal!</title>
-      </Head>
-      <Header />
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Welcome to skattframtal!</title>
+        </Head>
+        <Header />
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </ApolloProvider>
     </>
   )
 }
