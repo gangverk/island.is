76457c9cc42607c88e3e8ccc799587f85dce42a4
Jón Freysteinn Jónsson <nonni95@gmail.com>
2025-05-08 11:38:40 +0000

    feat: allow editing of bank account

--- apps/skattframtal/src/components/ActionLink.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css'
-
-export const iconWrap = style({
-  paddingLeft: 4,
-  display: 'inline-flex',
-  alignItems: 'center',
-  height: 16,
-  verticalAlign: 'middle',
-})
--- apps/skattframtal/src/pages/skattframtol/application/[taxReturnId]/my-information.tsx
+++ apps/skattframtal/src/pages/skattframtol/application/[taxReturnId]/my-information.tsx
@@ -1,22 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FormScreenLayout from '../../../../components/FormScreenLayout'
-import { Text, Box } from '@island.is/island-ui/core'
-import ActionLink from '../../../../components/ActionLink'
+import { Text, Box, Button, Input } from '@island.is/island-ui/core'
 import { useRouter } from 'next/router'
 import { stepKeys, stepLabels, goToStep } from '../../../../constants/formSteps'
-import { useQuery } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import { QUERIES } from '../../../../graphql/queries'
+import { MUTATIONS } from '../../../../graphql/mutations'
+
+type EditableField = 'emailAddress' | 'phoneNumber' | 'bankAccountNumber' | null
 
 export default function MyInformation() {
   const router = useRouter()
   const taxReturnId = router.query.taxReturnId as string
   const currentStepIndex = 1
 
+  // State for inline editing
+  const [editField, setEditField] = useState<EditableField>(null)
+  const [fieldValue, setFieldValue] = useState('')
+
   const { data, loading, error } = useQuery(
     QUERIES.GET_TAX_PAYER_BY_KENNITALA,
     {
       variables: { kennitala: '1203894569' },
     },
+  )
+
+  const [updateTaxPayer, { loading: updateTaxPayerLoading }] = useMutation(
+    MUTATIONS.UPDATE_TAX_PAYER,
+    {
+      onCompleted: () => {
+        // Clear edit state after successful update
+        setEditField(null)
+      },
+      refetchQueries: [
+        {
+          query: QUERIES.GET_TAX_PAYER_BY_KENNITALA,
+          variables: { kennitala: '1203894569' },
+        },
+      ],
+    },
+  )
+
+  const handleEditClick = (field: EditableField, value: string) => {
+    setEditField(field)
+    setFieldValue(value)
+  }
+
+  const handleCancel = () => {
+    setEditField(null)
+  }
+
+  const handleSave = () => {
+    if (editField && data?.taxPayerByKennitala) {
+      const variables: any = {
+        id: data.taxPayerByKennitala.taxPayerID,
+        phoneNumber: data.taxPayerByKennitala.phoneNumber || '',
+        emailAddress: data.taxPayerByKennitala.emailAddress || '',
+        bankAccountNumber: data.taxPayerByKennitala.bankAccountNumber || '',
+      }
+
+      // Update the field being edited
+      variables[editField] = fieldValue
+
+      updateTaxPayer({
+        variables,
+      })
+    }
+  }
+
+  const renderEditButtons = () => (
+    <Box display="flex">
+      <Box marginRight={1}>
+        <Button
+          size="small"
+          variant="ghost"
+          onClick={handleCancel}
+          disabled={updateTaxPayerLoading}
+        >
+          Hætta
+        </Button>
+      </Box>
+      <Button
+        size="small"
+        variant="primary"
+        onClick={handleSave}
+        loading={updateTaxPayerLoading}
+      >
+        Vista
+      </Button>
+    </Box>
   )
 
   return (
@@ -50,77 +122,196 @@ export default function MyInformation() {
             }}
           >
             <tbody>
-              {[
-                {
-                  label: 'Full nafn',
-                  value: data.taxPayerByKennitala.name,
-                  action: (
-                    <ActionLink href="skra.is">Breyta í þjóðskrá</ActionLink>
-                  ),
-                },
-                {
-                  label: 'Kennitala',
-                  value: data.taxPayerByKennitala.kennitala,
-                  action: null,
-                },
-                {
-                  label: 'Lögheimili',
-                  value: data.taxPayerByKennitala.address,
-                  action: (
-                    <ActionLink href="skra.is">Breyta í þjóðskrá</ActionLink>
-                  ),
-                },
-                {
-                  label: 'Netfang',
-                  value: data.taxPayerByKennitala.emailAddress,
-                  action: <ActionLink href="skra.is">Breyta</ActionLink>,
-                },
-                {
-                  label: 'Símanúmer',
-                  value: data.taxPayerByKennitala.phoneNumber,
-                  action: <ActionLink href="skra.is">Breyta</ActionLink>,
-                },
-                {
-                  label: 'Bankaupplýsingar',
-                  value: data.taxPayerByKennitala.bankAccountNumber,
-                  action: <ActionLink href="skra.is">Breyta</ActionLink>,
-                },
-              ].map((row, idx, arr) => {
-                const isLast = idx === arr.length - 1
-                const cellStyle = {
-                  borderBottom: isLast ? undefined : '1px solid #F2F7FF',
-                  background: '#fff',
-                  padding: '16px 8px',
+              {/* Basic info - not editable */}
+              <TableRow
+                label="Full nafn"
+                value={data.taxPayerByKennitala.name}
+                action={
+                  <Button
+                    size="small"
+                    variant="text"
+                    icon="open"
+                    iconType="outline"
+                  >
+                    Breyta í þjóðskrá
+                  </Button>
                 }
-                return (
-                  <tr key={row.label}>
-                    <th
-                      style={{
-                        ...cellStyle,
-                        textAlign: 'left',
-                        fontWeight: 600,
-                        width: 180,
-                      }}
+                isLast={false}
+              />
+              <TableRow
+                label="Kennitala"
+                value={data.taxPayerByKennitala.kennitala}
+                action={null}
+                isLast={false}
+              />
+              <TableRow
+                label="Lögheimili"
+                value={data.taxPayerByKennitala.address || ''}
+                action={
+                  <Button
+                    size="small"
+                    variant="text"
+                    icon="open"
+                    iconType="outline"
+                  >
+                    Breyta í þjóðskrá
+                  </Button>
+                }
+                isLast={false}
+              />
+
+              {/* Email - editable */}
+              <TableRow
+                label="Netfang"
+                value={data.taxPayerByKennitala.emailAddress || ''}
+                isEditing={editField === 'emailAddress'}
+                editValue={fieldValue}
+                onEditValueChange={setFieldValue}
+                action={
+                  editField === 'emailAddress' ? (
+                    renderEditButtons()
+                  ) : (
+                    <Button
+                      size="small"
+                      variant="text"
+                      icon="pencil"
+                      onClick={() =>
+                        handleEditClick(
+                          'emailAddress',
+                          data.taxPayerByKennitala.emailAddress || '',
+                        )
+                      }
                     >
-                      <Text variant="default" fontWeight="semiBold">
-                        {row.label}
-                      </Text>
-                    </th>
-                    <td style={cellStyle}>
-                      <Text>{row.value}</Text>
-                    </td>
-                    <td
-                      style={{ ...cellStyle, textAlign: 'right', width: 160 }}
+                      Breyta
+                    </Button>
+                  )
+                }
+                isLast={false}
+                loading={updateTaxPayerLoading}
+              />
+
+              {/* Phone number - editable */}
+              <TableRow
+                label="Símanúmer"
+                value={data.taxPayerByKennitala.phoneNumber || ''}
+                isEditing={editField === 'phoneNumber'}
+                editValue={fieldValue}
+                onEditValueChange={setFieldValue}
+                action={
+                  editField === 'phoneNumber' ? (
+                    renderEditButtons()
+                  ) : (
+                    <Button
+                      size="small"
+                      variant="text"
+                      icon="pencil"
+                      onClick={() =>
+                        handleEditClick(
+                          'phoneNumber',
+                          data.taxPayerByKennitala.phoneNumber || '',
+                        )
+                      }
                     >
-                      {row.action}
-                    </td>
-                  </tr>
-                )
-              })}
+                      Breyta
+                    </Button>
+                  )
+                }
+                isLast={false}
+                loading={updateTaxPayerLoading}
+              />
+
+              {/* Bank account - editable */}
+              <TableRow
+                label="Bankaupplýsingar"
+                value={data.taxPayerByKennitala.bankAccountNumber || ''}
+                isEditing={editField === 'bankAccountNumber'}
+                editValue={fieldValue}
+                onEditValueChange={setFieldValue}
+                action={
+                  editField === 'bankAccountNumber' ? (
+                    renderEditButtons()
+                  ) : (
+                    <Button
+                      size="small"
+                      variant="text"
+                      icon="pencil"
+                      onClick={() =>
+                        handleEditClick(
+                          'bankAccountNumber',
+                          data.taxPayerByKennitala.bankAccountNumber || '',
+                        )
+                      }
+                    >
+                      Breyta
+                    </Button>
+                  )
+                }
+                isLast={true}
+                loading={updateTaxPayerLoading}
+              />
             </tbody>
           </table>
         </Box>
       )}
     </FormScreenLayout>
+  )
+}
+
+interface TableRowProps {
+  label: string
+  value: string
+  action: React.ReactNode
+  isLast: boolean
+  isEditing?: boolean
+  editValue?: string
+  onEditValueChange?: (value: string) => void
+  loading?: boolean
+}
+
+const TableRow = ({
+  label,
+  value,
+  action,
+  isLast,
+  isEditing,
+  editValue,
+  onEditValueChange,
+  loading,
+}: TableRowProps) => {
+  const cellStyle = {
+    borderBottom: isLast ? undefined : '1px solid #F2F7FF',
+    background: '#fff',
+    padding: '16px 8px',
+  }
+
+  return (
+    <tr>
+      <th
+        style={{
+          ...cellStyle,
+          textAlign: 'left',
+          fontWeight: 600,
+          width: 180,
+        }}
+      >
+        <Text variant="default" fontWeight="semiBold">
+          {label}
+        </Text>
+      </th>
+      <td style={cellStyle}>
+        {isEditing && editValue !== undefined && onEditValueChange ? (
+          <Input
+            name={label}
+            value={editValue}
+            onChange={(e) => onEditValueChange(e.target.value)}
+            disabled={loading}
+            size="sm"
+          />
+        ) : (
+          <Text>{value}</Text>
+        )}
+      </td>
+      <td style={{ ...cellStyle, textAlign: 'right', width: 160 }}>{action}</td>
+    </tr>
   )
 }
