3f731e964e463c3859a5ac4333321670b22b5e2b
Jón Freysteinn Jónsson <nonni95@gmail.com>
2025-05-08 10:32:42 +0000

    feat: update and add

--- apps/skattframtal/src/pages/skattframtol/application/[taxReturnId]/income.tsx
+++ apps/skattframtal/src/pages/skattframtol/application/[taxReturnId]/income.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@island.is/island-ui/core'
+import { Box, Text, toast } from '@island.is/island-ui/core'
 import React from 'react'
 import { useRouter } from 'next/router'
 import FormScreenLayout from '../../../../components/FormScreenLayout'
@@ -6,9 +6,10 @@ import Table, { TableRowData } from '../../../../components/table/Table'
 import { stepKeys, stepLabels, goToStep } from '../../../../constants/formSteps'
 import { useMutation, useQuery } from '@apollo/client'
 import { QUERIES } from '../../../../graphql/queries'
-import { Money, TaxReturnIncomeCategory } from '../../../../graphql/schema'
+import { Money, TaxReturnIncome, TaxReturnIncomeCategory } from '../../../../graphql/schema'
 import { formatMoney, parseMoney } from '../../../../utils/money'
 import { MUTATIONS } from '../../../../graphql/mutations'
+import orderBy from 'lodash/orderBy'
 
 const IncomePage = () => {
   const router = useRouter()
@@ -18,7 +19,6 @@ const IncomePage = () => {
 
   const {
     data: incomeData,
-    loading,
     error,
   } = useQuery(QUERIES.GET_TAX_PAYER_INCOME_BY_TAX_RETURN_ID, {
     variables: { taxReturnId },
@@ -32,7 +32,7 @@ const IncomePage = () => {
     refetchQueries: [QUERIES.GET_TAX_PAYER_INCOME_BY_TAX_RETURN_ID],
   })
 
-  const income = incomeData?.taxReturnById.income
+  const income = orderBy(incomeData?.taxReturnById.income, 'payer', 'asc')
 
   const incomeSection = income?.filter(
     (income) => income.category === TaxReturnIncomeCategory.Salary,
@@ -70,8 +70,11 @@ const IncomePage = () => {
         title: 'Launatekjur og starfsgreindar greiðslur',
         sectionNumber: '2.1',
       },
+      defaultRawData: {
+        category: TaxReturnIncomeCategory.Salary,
+      },
       rows: incomeSection?.map((income) => ({
-        id: income.incomeID,
+        rawData: income,
         left: (
           <Box>
             <Box display="flex" alignItems="center">
@@ -93,8 +96,11 @@ const IncomePage = () => {
         title: 'Ökutækjastyrkur. Dagpeningar. Hlunnindi',
         sectionNumber: '2.2',
       },
+      defaultRawData: {
+        category: TaxReturnIncomeCategory.Benefit,
+      },
       rows: perDiemSection?.map((income) => ({
-        id: income.incomeID,
+        rawData: income,
         left: (
           <Box>
             <Box display="flex" alignItems="center">
@@ -117,10 +123,13 @@ const IncomePage = () => {
           'Lífeyrisgreiðslur. Greiðslur frá Tryggingastofnun. Aðrar bótagreiðslur, styrkir o.fl.',
         sectionNumber: '2.3',
       },
+      defaultRawData: {
+        category: TaxReturnIncomeCategory.Grant,
+      },
       rows: grantsSection?.map((income) => (
       [
         {
-          id: income.incomeID,
+          rawData: income,
           backgroundColor: 'purple100',
           leftValue: income.payer,
           left: (
@@ -133,7 +142,7 @@ const IncomePage = () => {
           middleValue: '',
         },
         {
-          id: income.incomeID,
+          rawData: income,
           left: (
             <Box display="flex" alignItems="center">
               <Text>{income.description}</Text>
@@ -143,36 +152,46 @@ const IncomePage = () => {
           rightValue: formatMoney(income.amount),
           middleValue: '',
         },
-      ] satisfies TableRowData[])).flat() || [],
+      ] satisfies TableRowData<TaxReturnIncome>[])).flat() || [],
       sum: grantsSectionSum ? formatMoney(grantsSectionSum) : '0',
     },
   ]
 
-  const handleChange = async (values: { id: string, left: string; middle: string; right: string }) => {
-    if (values.id) {
-      await updateIncome({
-        variables: {
-          incomeId: values.id,
-          input: {
-            amount: parseMoney(values.right),
-            category: TaxReturnIncomeCategory.Salary,
-            description: values.middle,
-            payer: values.left,
+  const handleChange = async (values: { rawData: TaxReturnIncome, left: string; middle: string; right: string }) => {
+    let success = false
+    try {
+      if (values.rawData.incomeID) {
+        const result = await updateIncome({
+          variables: {
+            incomeId: values.rawData.incomeID,
+            input: {
+              amount: parseMoney(values.right),
+              category: values.rawData.category,
+              description: values.middle,
+              payer: values.left,
+            },
           },
-        },
-      })
-    } else {
-      await addIncome({
-        variables: {
-          taxReturnId,
-          input: {
-            amount: parseMoney(values.right),
-            category: TaxReturnIncomeCategory.Benefit,
-            description: values.middle,
-            payer: values.left,
+        })
+        success = !!result.data?.updateTaxReturnIncome.incomeID
+      } else {
+        const result = await addIncome({
+          variables: {
+            taxReturnId,
+            input: {
+              amount: parseMoney(values.right),
+              category: values.rawData.category,
+              description: values.middle,
+              payer: values.left,
+            },
           },
-        },
-      })
+        })
+        success = !!result.data?.addTaxReturnIncome.incomeID
+      }
+      if (!success) {
+        toast.error('Villa í uppfærslu')
+      }
+    } catch (error) {
+      toast.error('Villa í uppfærslu')
     }
   }
 
@@ -187,9 +206,8 @@ const IncomePage = () => {
       <Text as="h1" variant="h1" marginBottom={6}>
         Tekjur ársins 2024
       </Text>
-      {loading && <Text>Hleður gögnum...</Text>}
       {error && <Text>Villa kom upp: {error.message}</Text>}
-      {incomeData && <Table key={incomeSectionSum?.amount} data={tableData} onChange={handleChange} />}
+      {incomeData && <Table data={tableData} onChange={handleChange} />}
     </FormScreenLayout>
   )
 }
